744e42b80c54e798682df726f4f9b643b7ef9b22
Marko K. S. <marko.seppanen@gmail.com>
2025-04-26 01:24:01 +0200

    Added scripts

---
+++ antl3-tool.sh
@@ -0,0 +1,4 @@
+# Type in bash shell:
+# 	bash antl3-tool.sh
+
+java -jar libs/antlr4/antlr-4.13.2-complete.jar
--- test.sh
+++ test.sh
@@ -2,12 +2,40 @@
 
 #thelib = "antlr-4.13.2-complete.jar"
 
-cd Libs/ANTLR4
+#cd Libs/ANTLR4
 
-java -jar "antlr-4.13.2-complete.jar"
+#java -jar "antlr-4.13.2-complete.jar"
 #java -jar $thelib
 
-cd Grammar-ANTLR4/
+#cd Grammar-ANTLR4/
 
 #antlr4-parse yini.g4 Samples/Good/Advanced.yini yini -tree
 #antlr4-parse yini.g4 yini -tree
+
+# java -jar libs/antlr4/antlr-4.13.2-complete.jar  specs/v1.0.0-beta.3/YiniParser.g4 -tree
+
+# antlr4-parse specs/v1.0.0-beta.3/YiniParser.g4 specs/v1.0.0-beta.3/YiniLexer.g4 sample.yini -tree
+
+# antlr4-parse specs/v1.0.0-beta.3/YiniParser.g4 specs/v1.0.0-beta.3/YiniLexer.g4 yini sample.yini -tokens
+
+echo Runnish this script in Bash...
+
+VER_STR="v1.0.0-beta.3"
+
+SPECS_BASE="specs"
+
+PARSER_FILE="YiniParser.g4"
+LEXER_FILE="YiniLexer.g4"
+
+PATH_PARSER=$SPECS_BASE/$VER_STR/$PARSER_FILE
+PATH_LEXER=$SPECS_BASE/$VER_STR/$LEXER_FILE
+
+START_RULE="yini"
+
+echo Parser file including path: $PATH_PARSER
+echo Parser file including path: $PATH_LEXER
+
+antlr4-parse $PATH_PARSER $PATH_LEXER $START_RULE sample.yini -tokens
+
+
+
