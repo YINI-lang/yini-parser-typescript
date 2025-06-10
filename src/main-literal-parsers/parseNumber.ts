16f9b9cee70668e9e645d37160f27c7c5df2f515
Marko K. S. <marko.seppanen@gmail.com>
2025-06-11 00:49:29 +0200

    Fixed issue where basic int numbers failed

--- src/main-literal-parsers/parseNumber.ts
+++ src/main-literal-parsers/parseNumber.ts
@@ -31,7 +31,7 @@ const parseNumberLiteral = (
     }
 
     // TODO: Depending, on mode, below continue or break on error
-    console.error('Error: Failed to parse number value: ' + txt)
+    //console.error('Error: Failed to parse number value: ' + txt)
 
     return { type: 'Number-Integer', value: parseInt(txt) }
 }
