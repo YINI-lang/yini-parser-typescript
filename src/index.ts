06a08ad93263cd6e204c44ef9ce5851b2e25d0a0
Marko K. S. <marko.seppanen@gmail.com>
2025-07-31 22:18:06 +0200

    Added more golden tests for integers and floats

--- src/index.ts
+++ src/index.ts
@@ -206,8 +206,12 @@ Expected JS output:
         // = "missing_key_name"  // In strict should throw error, while lenient should pass
         //         `
         const yini = `
-^ App
-str = "Hello\\World"
+^ Numbers
+float9  = -9.999e+99         // Large negative float, scientific
+--float10 = 0.000000000000000000001  // Smallest normal value you expect to support
+--float11 = -0.000000000000000000001 // Negative small
+float12 = 2.2250738585072014e-308  // Smallest positive normal double
+float13 = -1.7976931348623157e+308 // Min (negative) JS double
     `
 
         // YINI.parse(yini, false)
