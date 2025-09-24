ee6ee1699d80b329a18470e4603b66a4c2c3bcb1
Marko K. S. <marko.seppanen@gmail.com>
2025-09-24 19:35:30 +0200

    Enabled Jest coverage test/report

--- jest.config.js
+++ jest.config.js
@@ -12,5 +12,27 @@ module.exports = {
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
-    testPathIgnorePatterns: ['/dist/', '/node_modules/'],
+    testPathIgnorePatterns: [
+        '/dist/',
+        '/node_modules/',
+        '/src/grammar/generated/',
+    ],
+    testEnvironment: 'node',
+    coverageProvider: 'v8',
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!src/grammar/generated/**',
+        '!**/*.d.ts',
+    ],
+    coverageReporters: ['text', 'html', 'lcov'],
+    coveragePathIgnorePatterns: [
+        '/dist/',
+        '/node_modules/',
+        '/src/grammar/generated/',
+    ],
+    // Optional thresholds
+    coverageThreshold: {
+        global: { branches: 80, functions: 85, lines: 85, statements: 85 },
+    },
 }
